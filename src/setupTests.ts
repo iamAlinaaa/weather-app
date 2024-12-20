0b7cd125287ca62aee43ead3585e3e99a0b2759b
iamAlinaaa <alinaaevns1806@gmail.com>
2024-12-20 16:15:34 +0200

    deleted unsused compoents

--- src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'; 
-import { TextEncoder } from 'util';
-global.TextEncoder = TextEncoder;
-
-process.env.VITE_OPEN_WEATHER_API_KEY = '37a3c2d23245d57d2ae3cd675fa693b9';
